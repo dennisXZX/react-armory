667a26993816dcbdcece96f424bcaf871a629ea4
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-09-06 13:38:57 +1000

    complete header component

--- src/app-clones/airbnb/components/Header/Header.js
+++ src/app-clones/airbnb/components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Header.scss';
 import logo from '../../assets/airbnb-logo.png'
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Avatar from '@material-ui/core/Avatar';
 
 const Header = () => {
     return (
@@ -10,6 +14,18 @@ const Header = () => {
                 src={logo}
                 alt="Airbnb logo"
             />
+
+            <div className='header__center'>
+                <input type="text" />
+                <SearchIcon />
+            </div>
+
+            <div className='header__right'>
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+            </div>
         </div>
     );
 };
