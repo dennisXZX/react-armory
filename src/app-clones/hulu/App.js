4b0854fd53a8d6b1a0a7a8e7a481f26975016375
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-09-08 10:55:03 +1000

    complete Nav

--- src/app-clones/hulu/App.js
+++ src/app-clones/hulu/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.module.scss';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 
 const App = () => {
     return (
         <div className={styles.app}>
             <Header />
+            <Nav />
         </div>
     );
 };
