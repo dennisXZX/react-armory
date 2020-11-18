e050d2c6222fa72bce149f88846ab60cbb5835ef
Dennis Xiao <dennisboys@gmail.com>
2020-11-18 10:36:52 +1000

    refactor folder structure

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Homepage.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.scss";
 
 const Homepage = () => {
-    return (
-        <div className={styles.homepage}>
-            <section className={styles.section}>
-                <h1>App Clones</h1>
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.section}>
+        <h1>Apps</h1>
 
-                <ul>
-                    <li><Link to="/instagram">Instagram Reels</Link></li>
-                    <li><Link to="/facebook">Facebook</Link></li>
-                    <li><Link to="/airbnb">Airbnb</Link></li>
-                    <li><Link to="/hulu">Hulu</Link></li>
-                    <li><Link to="/covid-v1">Covid v1</Link></li>
-                </ul>
-            </section>
+        <ul>
+          <li>
+            <Link to="/instagram">Instagram Reels</Link>
+          </li>
+          <li>
+            <Link to="/facebook">Facebook</Link>
+          </li>
+          <li>
+            <Link to="/airbnb">Airbnb</Link>
+          </li>
+          <li>
+            <Link to="/hulu">Hulu</Link>
+          </li>
+          <li>
+            <Link to="/covid-v1">Covid v1</Link>
+          </li>
+        </ul>
+      </section>
 
-            <section className={styles.section}>
-                <h1>Components</h1>
+      <section className={styles.section}>
+        <h1>Components</h1>
 
-                <ul>
-                    <li><Link to="/card-v1">Card v1</Link></li>
-                    <li><Link to="/menu-v1">Menu v1</Link></li>
-                    <li><Link to="/menu-v2">Menu v2</Link></li>
-                    <li><Link to="/skeleton-v1">Skeleton v1</Link></li>
-                    <li><Link to="/skeleton-v2">Skeleton v2</Link></li>
-                </ul>
-            </section>
-        </div>
-    );
+        <ul>
+          <li>
+            <Link to="/card-v1">Card v1</Link>
+          </li>
+          <li>
+            <Link to="/menu-v1">Menu v1</Link>
+          </li>
+          <li>
+            <Link to="/menu-v2">Menu v2</Link>
+          </li>
+          <li>
+            <Link to="/skeleton-v1">Skeleton v1</Link>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,82 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.module.scss';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Facebook from './app-clones/facebook';
-import Airbnb from './app-clones/airbnb';
-import Homepage from './homepage/Homepage';
-import Hulu from './app-clones/hulu';
-import Instagram from './app-clones/instagram-reels';
-import MenuV1 from './component-gallery/menu-v1/MenuV1';
-import Wrapper from './homepage/components/Wrapper/Wrapper';
-import Cards from './component-gallery/card-v1/Cards';
-import CovidV1 from './app-clones/covid-v1';
-import MenuV2 from './component-gallery/menu-v2/MenuV2';
-import SkeletonV1 from './component-gallery/skeleton-v1/SkeletonV1';
-import SkeletonV2 from './component-gallery/skeleton-v2/SkeletonV2';
+import Airbnb from "./apps/airbnb";
+import CovidV1 from "./apps/covid-v1";
+import Facebook from "./apps/facebook";
+import Hulu from "./apps/hulu";
+import Instagram from "./apps/instagram-reels";
+import Cards from "./components/card-v1/Cards";
+import MenuV1 from "./components/menu-v1/MenuV1";
+import MenuV2 from "./components/menu-v2/MenuV2";
+import SkeletonV1 from "./components/skeleton-v1/SkeletonV1";
+import Wrapper from "./homepage/components/Wrapper/Wrapper";
+import Homepage from "./homepage/Homepage";
+import "./index.module.scss";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Switch>
-                {/* App Clones */}
-                <Route path="/instagram">
-                    <Instagram />
-                </Route>
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        {/* Apps */}
+        <Route path="/instagram">
+          <Instagram />
+        </Route>
 
-                <Route path="/facebook">
-                    <Facebook />
-                </Route>
+        <Route path="/facebook">
+          <Facebook />
+        </Route>
 
-                <Route path="/airbnb">
-                    <Airbnb />
-                </Route>
+        <Route path="/airbnb">
+          <Airbnb />
+        </Route>
 
-                <Route path="/hulu">
-                    <Hulu />
-                </Route>
+        <Route path="/hulu">
+          <Hulu />
+        </Route>
 
-                <Route path="/covid-v1">
-                    <CovidV1 />
-                </Route>
+        <Route path="/covid-v1">
+          <CovidV1 />
+        </Route>
 
-                {/* Components */}
-                <Route path="/card-v1">
-                    <Wrapper bgColor="#17141d"><Cards /></Wrapper>
-                </Route>
+        {/* Components */}
+        <Route path="/card-v1">
+          <Wrapper bgColor="#17141d">
+            <Cards />
+          </Wrapper>
+        </Route>
 
-                <Route path="/menu-v1">
-                    <Wrapper bgColor="#080b1f"><MenuV1 /></Wrapper>
-                </Route>
+        <Route path="/menu-v1">
+          <Wrapper bgColor="#080b1f">
+            <MenuV1 />
+          </Wrapper>
+        </Route>
 
-                <Route path="/menu-v2">
-                    <Wrapper bgColor="#080b1f"><MenuV2 /></Wrapper>
-                </Route>
+        <Route path="/menu-v2">
+          <Wrapper bgColor="#080b1f">
+            <MenuV2 />
+          </Wrapper>
+        </Route>
 
-                <Route path="/skeleton-v1">
-                    <Wrapper bgColor="#eee" direction="column">
-                        <SkeletonV1 circle={true} />
-                        <SkeletonV1 />
-                        <SkeletonV1 />
-                        <SkeletonV1 />
-                    </Wrapper>
-                </Route>
+        <Route path="/skeleton-v1">
+          <Wrapper bgColor="#eee" direction="column">
+            <SkeletonV1 circle={true} />
+            <SkeletonV1 />
+            <SkeletonV1 />
+            <SkeletonV1 />
+          </Wrapper>
+        </Route>
 
-                <Route path="/skeleton-v2">
-                    <Wrapper bgColor="#eee">
-                        <SkeletonV2 />
-                    </Wrapper>
-                </Route>
-
-                {/* Default route */}
-                <Route path="/">
-                    <Homepage />
-                </Route>
-            </Switch>
-        </Router>
-    </React.StrictMode>,
-    document.getElementById('root')
+        {/* Default route */}
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
